29c1d14eb65334c5af3e25396be405e9146adf09
Ardhi Lukianto <ardhi@lukianto.com>
2019-01-25 11:17:52 +0700

    reg update

--- cuks/core/helper/add_rest_access.js
+++ cuks/core/helper/add_rest_access.js
@@ -1,14 +1,14 @@
 'use strict'
 
 module.exports = function (cuk) {
-  return (access, pkgName, resource) => {
+  return (access, pkgName, resource, attribs) => {
     access
-      .createAny(`rest:${pkgName}:create:${resource}`)
-      .readAny(`rest:${pkgName}:find:${resource}`)
-      .readAny(`rest:${pkgName}:findOne:${resource}`)
-      .updateAny(`rest:${pkgName}:replace:${resource}`)
-      .updateAny(`rest:${pkgName}:modify:${resource}`)
-      .deleteAny(`rest:${pkgName}:remove:${resource}`)
+      .createAny(`rest:${pkgName}:create:${resource}`, attribs || ['*'])
+      .readAny(`rest:${pkgName}:find:${resource}`, attribs || ['*'])
+      .readAny(`rest:${pkgName}:findOne:${resource}`, attribs || ['*'])
+      .updateAny(`rest:${pkgName}:replace:${resource}`, attribs || ['*'])
+      .updateAny(`rest:${pkgName}:modify:${resource}`, attribs || ['*'])
+      .deleteAny(`rest:${pkgName}:remove:${resource}`, attribs || ['*'])
     return access
   }
 }
